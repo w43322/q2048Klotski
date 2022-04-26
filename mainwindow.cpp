5ac7663e58c8a90fb1ee5b2c72764e03b3110730
w43322 <raywang777@foxmail.com>
2022-04-26 15:17:11 +0800

    Update mainwindow.cpp
    加入方向键

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,16 +18,16 @@ void MainWindow::keyPressEvent(QKeyEvent *event)
 {
     switch (event->key())
     {
-        case Qt::Key_W:
+        case Qt::Key_W:case Qt::Key_Up:
             GAME.Step('w');
             break;
-        case Qt::Key_A:
+        case Qt::Key_A:case Qt::Key_Left:
             GAME.Step('a');
             break;
-        case Qt::Key_S:
+        case Qt::Key_S:case Qt::Key_Down:
             GAME.Step('s');
             break;
-        case Qt::Key_D:
+        case Qt::Key_D:case Qt::Key_Right:
             GAME.Step('d');
             break;
     default:
