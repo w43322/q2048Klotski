8959add1a696f101656eb3c019169f0a0ab0da3f
w43322 <raywang777@foxmail.com>
2022-05-24 10:56:03 +0800

    include font

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "mainwindow/mainwindow.h"
 
 #include <QApplication>
+#include <QFontDatabase>
 
 int main(int argc, char *argv[])
 {
+    QFontDatabase::addApplicationFont(":/fonts/menlo.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/monaco.ttf");
     QApplication a(argc, argv);
     //a.setQuitOnLastWindowClosed(false);
     MainWindow w;
