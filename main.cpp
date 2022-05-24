193249d10635896533f733bd8ef297e7548a33d3
w43322 <raywang777@foxmail.com>
2022-05-24 10:31:15 +0800

    disable setquit

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    a.setQuitOnLastWindowClosed(false);
+    //a.setQuitOnLastWindowClosed(false);
     MainWindow w;
     w.show();
     return a.exec();
